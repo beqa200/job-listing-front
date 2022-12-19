41962c55cb163878706d020e4b37aeae785aa471
beqa <beqamaisuradze912@gmail.com>
2022-12-19 14:47:21 +0400

    style job boxes

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,38 +1,38 @@
 import { JobWrapper } from "../styled-components";
-
-import {useState} from "react";
+import { useState } from "react";
 
 const Jobs = (props: JobPropsType) => {
- 
-  
   return (
-    <JobWrapper>
+    <JobWrapper new={props.job.new} featured={props.job.featured}>
       <section className="section1">
-        <img src={process.env.PUBLIC_URL + props.job.logo}/>
-        <div>
-          <p>{props.job.company}</p>
-          <p>{props.job.new == true ? "NEW!" : null}</p>
-          <p>{props.job.featured == true ? "FEATURED" : null}</p>
+        <img src={process.env.PUBLIC_URL + props.job.logo} />
+        <div className="for-flex1">
+          <p className="company">{props.job.company}</p>
+          <p className="new">{props.job.new == true ? "NEW!" : null}</p>
+          <p className="featured">
+            {props.job.featured == true ? "FEATURED" : null}
+          </p>
         </div>
 
-        <p>{props.job.position}</p>
+        <p className="position">{props.job.position}</p>
 
-        <div>
-            <p>{props.job.postedAt}</p>
-            <p>{props.job.contract}</p>
-            <p>{props.job.location}</p>
-
+        <div className="for-flex2">
+          <p>{props.job.postedAt}</p>
+          <ul>
+            <li>{props.job.contract}</li>
+            <li>{props.job.location}</li>
+          </ul>
         </div>
-        <section className="section2">
-            <p>{props.job.role}</p>
-            <p>{props.job.level}</p>
-            {props.job.languages.map((language: String) => {
-                return <p>{language}</p>
-            })}
-            {props.job.tools.map((tool: String) => {
-                return <p>{tool}</p>
-            })}
-        </section>
+      </section>
+      <section className="section2">
+        <p>{props.job.role}</p>
+        <p>{props.job.level}</p>
+        {props.job.languages.map((language: String) => {
+          return <p>{language}</p>;
+        })}
+        {props.job.tools.map((tool: String) => {
+          return <p>{tool}</p>;
+        })}
       </section>
     </JobWrapper>
   );
--- src/styled-components/JobWrapper.styled.ts
+++ src/styled-components/JobWrapper.styled.ts
@@ -1,9 +1,98 @@
 import styled from "styled-components";
 
+type PropsTypes = {
+  new: Boolean;
+  featured: Boolean;
+};
+
 const JobWrapper = styled.div`
-    width: 90%;
-    height: 257px;
-    background-color: white;
-`
+  width: calc(90% - 48px);
+  height: 240px;
+  padding: 0px 24px;
+  background-color: white;
+  margin: 40px auto;
+  border-radius: 5px;
+  box-shadow: 0px 15px 20px -5px rgba(13, 113, 130, 0.15);
+  border-left: ${(props: PropsTypes) =>
+    props.new == true && props.featured == true ? "5px solid #5CA5A5" : ""};
+
+  .section1 {
+    border-bottom: 1px solid #b7c4c4;
+    img {
+      width: 40px;
+      margin-top: -20px;
+    }
+
+    .for-flex1 {
+      display: flex;
+      align-items: center;
+
+      font-size: 14px;
+      color: white;
+      .company {
+        font-size: 13px;
+        color: #5ca5a5;
+        margin-top: 10px;
+        height: 24px;
+      }
+
+      .new {
+        background-color: ${(props: PropsTypes) =>
+          props.new == true ? "#5CA5A5" : ""};
+        border-radius: 12px;
+        padding: 7px 8px 3px 7px;
+        margin-left: 33px;
+      }
+
+      .featured {
+        background-color: ${(props: PropsTypes) =>
+          props.featured == true ? "#2b3939" : ""};
+        border-radius: 12px;
+        padding: 7px 8px 3px 7px;
+        margin-left: 8px;
+      }
+    }
+
+    .position {
+      margin-top: 9px;
+      font-size: 15px;
+      color: #2b3939;
+    }
+
+    .for-flex2 {
+      display: flex;
+      font-size: 16px;
+      color: #7c8f8f;
+      margin-top: 10px;
+      width: 260px;
+      padding-bottom: 15px;
+      ul {
+        display: flex;
+        width: 180px;
+
+        li {
+          margin-left: 23px;
+        }
+      }
+    }
+  }
+
+  .section2 {
+    font-size: 18px;
+    color: #5ca5a5;
+    font-weight: 700;
+  
+   
+
+    p {
+      margin-top: 16px;
+      margin-right: 16px;
+      display: inline-block;
+      padding: 5px 11px 3px 8px;
+      background-color: rgba(92, 165, 165, 0.1);
+      border-radius: 4px;
+    }
+  }
+`;
 
 export default JobWrapper;
